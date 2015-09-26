1580083df7427f238762dbbe1843c8626fd28d44
Smrutirekha Dash <mishra.bishnu@gmail.com>
2015-09-26 16:43:27 +0530

    Test commit

---
+++ TestServlet/src/Trial/servlet/TestServlet.java
@@ -0,0 +1,22 @@
+package Trial.servlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+public class TestServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request,HttpServletResponse response)
+	throws ServletException, IOException{
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD>");
+		out.println("<TITLE>Servlet</TITLE>");
+		out.println("</HEAD>");
+		out.println("<BODY>");
+		out.println("Welcome to Servlet Testing");
+		out.println("</BODY>");
+		out.println("</HTML>");
+		
+	}
+}
